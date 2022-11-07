13d142c96f66b28a3440001409692e6937e1463d
DandunGP <dandun.gigih.prakoso@gmail.com>
2022-11-07 18:21:42 +0700

    [push] all controller and middleware

--- routes/routes.go
+++ routes/routes.go
@@ -1,93 +1,89 @@
 package routes
 
 import (
+	"erp/constants"
 	"erp/controllers"
 
 	"github.com/labstack/echo"
+	mid "github.com/labstack/echo/middleware"
 )
 
 func New() *echo.Echo {
 	e := echo.New()
 
-	e.GET("/users", controllers.GetUsersController)
-	e.GET("/users/:id", controllers.GetUserController)
-	e.POST("/users", controllers.CreateUserController)
-	e.DELETE("/users/:id", controllers.DeleteUserController)
-	e.PUT("/users/:id", controllers.UpdateUserController)
+	eUser := e.Group("users")
+	eUser.Use(mid.JWT([]byte(constants.SECRET_KEY)))
 
-	e.GET("/officers", controllers.GetOfficersController)
-	e.GET("/officers/:id", controllers.GetOfficerController)
-	e.POST("/officers", controllers.CreateOfficerController)
-	e.DELETE("/officers/:id", controllers.DeleteOfficerController)
-	e.PUT("/officers/:id", controllers.UpdateOfficerController)
+	eUser.GET("", controllers.GetUsersController)
+	eUser.GET("/:id", controllers.GetUserController)
+	eUser.POST("", controllers.CreateUserController)
+	eUser.DELETE("/:id", controllers.DeleteUserController)
+	eUser.PUT("/:id", controllers.UpdateUserController)
 
-	e.GET("/gudangs", controllers.GetGudangsController)
-	e.GET("/gudangs/:id", controllers.GetGudangController)
-	e.POST("/gudangs", controllers.CreateGudangController)
-	e.DELETE("/gudangs/:id", controllers.DeleteGudangController)
-	e.PUT("/gudangs/:id", controllers.UpdateGudangController)
+	eOfficer := e.Group("officers")
+	eOfficer.Use(mid.JWT([]byte(constants.SECRET_KEY)))
 
-	e.GET("/persediaan_barang", controllers.GetPersediaansController)
-	e.GET("/persediaan_barang/:id", controllers.GetPersediaanController)
-	e.POST("/persediaan_barang", controllers.CreatePersediaanController)
-	e.DELETE("/persediaan_barang/:id", controllers.DeletePersediaanController)
-	e.PUT("/persediaan_barang/:id", controllers.UpdatePersediaanController)
+	eOfficer.GET("", controllers.GetOfficersController)
+	eOfficer.GET("/:id", controllers.GetOfficerController)
+	eOfficer.POST("", controllers.CreateOfficerController)
+	eOfficer.DELETE("/:id", controllers.DeleteOfficerController)
+	eOfficer.PUT("/:id", controllers.UpdateOfficerController)
 
-	e.GET("/barang_masuk", controllers.GetBarangsController)
-	e.GET("/barang_masuk/:id", controllers.GetBarangController)
-	e.POST("/barang_masuk", controllers.CreateBarangController)
-	e.DELETE("/barang_masuk/:id", controllers.DeleteBarangController)
-	e.PUT("/barang_masuk/:id", controllers.UpdateBarangController)
+	eGudang := e.Group("gudangs")
+	eGudang.Use(mid.JWT([]byte(constants.SECRET_KEY)))
 
-	e.GET("/bom", controllers.GetBOMsController)
-	e.GET("/bom/:id", controllers.GetBOMController)
-	e.POST("/bom", controllers.CreateBOMController)
-	e.DELETE("/bom/:id", controllers.DeleteBOMController)
-	e.PUT("/bom/:id", controllers.UpdateBOMController)
+	eGudang.GET("", controllers.GetGudangsController)
+	eGudang.GET("/:id", controllers.GetGudangController)
+	eGudang.POST("", controllers.CreateGudangController)
+	eGudang.DELETE("/:id", controllers.DeleteGudangController)
+	eGudang.PUT("/:id", controllers.UpdateGudangController)
 
-	e.GET("/wo", controllers.GetWOsController)
-	e.GET("/wo/:id", controllers.GetWOController)
-	e.POST("/wo", controllers.CreateWOController)
-	e.DELETE("/wo/:id", controllers.DeleteWOController)
-	e.PUT("/wo/:id", controllers.UpdateWOController)
+	ePersediaan := e.Group("persediaan_barang")
+	ePersediaan.Use(mid.JWT([]byte(constants.SECRET_KEY)))
 
-	e.GET("/produk", controllers.GetProduksController)
-	e.GET("/produk/:id", controllers.GetProdukController)
-	e.POST("/produk", controllers.CreateProdukController)
-	e.DELETE("/produk/:id", controllers.DeleteProdukController)
-	e.PUT("/produk/:id", controllers.UpdateProdukController)
+	ePersediaan.GET("", controllers.GetPersediaansController)
+	ePersediaan.GET("/:id", controllers.GetPersediaanController)
+	ePersediaan.POST("", controllers.CreatePersediaanController)
+	ePersediaan.DELETE("/:id", controllers.DeletePersediaanController)
+	ePersediaan.PUT("/:id", controllers.UpdatePersediaanController)
+
+	eBarang := e.Group("barang_masuk")
+	eBarang.Use(mid.JWT([]byte(constants.SECRET_KEY)))
+
+	eBarang.GET("", controllers.GetBarangsController)
+	eBarang.GET("/:id", controllers.GetBarangController)
+	eBarang.POST("", controllers.CreateBarangController)
+	eBarang.DELETE("/:id", controllers.DeleteBarangController)
+	eBarang.PUT("/:id", controllers.UpdateBarangController)
+
+	eBom := e.Group("bom")
+	eBom.Use(mid.JWT([]byte(constants.SECRET_KEY)))
+
+	eBom.GET("", controllers.GetBOMsController)
+	eBom.GET("/:id", controllers.GetBOMController)
+	eBom.POST("", controllers.CreateBOMController)
+	eBom.DELETE("/:id", controllers.DeleteBOMController)
+	eBom.PUT("/:id", controllers.UpdateBOMController)
+
+	eWo := e.Group("wo")
+	eWo.Use(mid.JWT([]byte(constants.SECRET_KEY)))
+
+	eWo.GET("", controllers.GetWOsController)
+	eWo.GET("/:id", controllers.GetWOController)
+	eWo.POST("", controllers.CreateWOController)
+	eWo.DELETE("/:id", controllers.DeleteWOController)
+	eWo.PUT("/:id", controllers.UpdateWOController)
+
+	eProduk := e.Group("produk")
+	eProduk.Use(mid.JWT([]byte(constants.SECRET_KEY)))
+
+	eProduk.GET("", controllers.GetProduksController)
+	eProduk.GET("/:id", controllers.GetProdukController)
+	eProduk.POST("", controllers.CreateProdukController)
+	eProduk.DELETE("/:id", controllers.DeleteProdukController)
+	eProduk.PUT("/:id", controllers.UpdateProdukController)
 
 	e.POST("/login", controllers.LoginController)
 
-	// eJwtAuth := e.Group("auth")
-	// eJwtAuth.Use(middleware.checkAdmin())
-	// eJwtAuth.GET("/wo", controllers.GetWOsController)
-	// eJwtAuth.GET("/users/:id", controllers.GetUserController)
-	// eJwtAuth.GET("/books", controllers.GetBooksController)
-	// eJwtAuth.GET("/books/:id", controllers.GetBookController)
-
-	// eJwtAuth.Use(mid.JWT([]byte(constants.SECRET_KEY)))
-	// eJwtAuth.GET("/users", controllers.GetUsersController)
-	// eJwtAuth.POST("/users", controllers.CreateUserController)
-	// eJwtAuth.DELETE("/users/:id", controllers.DeleteUserController)
-	// eJwtAuth.PUT("/users/:id", controllers.UpdateUserController)
-
-	// eJwtAuth.POST("/books", controllers.CreateBookController)
-	// eJwtAuth.DELETE("/books/:id", controllers.DeleteBookController)
-	// eJwtAuth.PUT("/books/:id", controllers.UpdateBookController)
 	return e
 }
-
-/*
-	e.GET("/users", controllers.GetUsersController)
-	e.GET("/users/:id", controllers.GetUserController)
-	e.POST("/users", controllers.CreateUserController)
-	e.DELETE("/users/:id", controllers.DeleteUserController)
-	e.PUT("/users/:id", controllers.UpdateUserController)
-
-	e.GET("/books", controllers.GetBooksController)
-	e.GET("/books/:id", controllers.GetBookController)
-	e.POST("/books", controllers.CreateBookController)
-	e.DELETE("/books/:id", controllers.DeleteBookController)
-	e.PUT("/books/:id", controllers.UpdateBookController)
-*/
